7cea06925aeada488880aa2089bd3b479eeea0e7
Juan Acosta Quiñones <juaanx7@gmail.com>
2022-09-30 16:30:52 -0300

    Proyecto de facultad para gestion de tareas diarias

---
+++ dashboardAdmin/main.js
@@ -0,0 +1,223 @@
+$(document).ready(function(){
+    tablaPersonas = $("#tablaPersonas").DataTable({
+       "columnDefs":[{
+        "targets": -1,
+        "data":null,
+        "defaultContent": "<div class='text-center'><div class='btn-group'><button class='btn btn-primary btnEditar'>Editar</button><button class='btn btn-danger btnBorrar'>Borrar</button></div></div>"  
+       }],
+        
+    "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sSearch": "Buscar:",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast":"Último",
+                "sNext":"Siguiente",
+                "sPrevious": "Anterior"
+             },
+             "sProcessing":"Procesando...",
+        }
+    });
+    
+$("#btnNuevo").click(function(){
+    $("#formPersonas").trigger("reset");
+    $(".modal-header").css("background-color", "#1cc88a");
+    $(".modal-header").css("color", "white");
+    $(".modal-title").text("Nueva Persona");            
+    $("#modalCRUD").modal("show");        
+    id=null;
+    opcion = 1; //alta
+});    
+    
+var fila; //capturar la fila para editar o borrar el registro
+    
+//botón EDITAR    
+$(document).on("click", ".btnEditar", function(){
+    fila = $(this).closest("tr");
+    id = parseInt(fila.find('td:eq(0)').text());
+    nombre = fila.find('td:eq(1)').text();
+    pais = fila.find('td:eq(2)').text();
+    edad = parseInt(fila.find('td:eq(3)').text());
+    
+    $("#nombre").val(nombre);
+    $("#pais").val(pais);
+    $("#edad").val(edad);
+    opcion = 2; //editar
+    
+    $(".modal-header").css("background-color", "#4e73df");
+    $(".modal-header").css("color", "white");
+    $(".modal-title").text("Editar Persona");            
+    $("#modalCRUD").modal("show");  
+    
+});
+
+//botón BORRAR
+$(document).on("click", ".btnBorrar", function(){    
+    fila = $(this);
+    id = parseInt($(this).closest("tr").find('td:eq(0)').text());
+    opcion = 3 //borrar
+    var respuesta = confirm("¿Está seguro de eliminar el registro: "+id+"?");
+    if(respuesta){
+        $.ajax({
+            url: "bd/crud.php",
+            type: "POST",
+            dataType: "json",
+            data: {opcion:opcion, id:id},
+            success: function(){
+                tablaPersonas.row(fila.parents('tr')).remove().draw();
+            }
+        });
+    }   
+});
+    
+$("#formPersonas").submit(function(e){
+    e.preventDefault();    
+    nombre = $.trim($("#nombre").val());
+    pais = $.trim($("#pais").val());
+    edad = $.trim($("#edad").val());    
+    $.ajax({
+        url: "bd/crud.php",
+        type: "POST",
+        dataType: "json",
+        data: {nombre:nombre, pais:pais, edad:edad, id:id, opcion:opcion},
+        success: function(data){  
+            console.log(data);
+            id = data[0].id;            
+            nombre = data[0].nombre;
+            pais = data[0].pais;
+            edad = data[0].edad;
+            if(opcion == 1){tablaPersonas.row.add([id,nombre,pais,edad]).draw();}
+            else{tablaPersonas.row(fila).data([id,nombre,pais,edad]).draw();}            
+        }        
+    });
+    $("#modalCRUD").modal("hide");    
+    
+});    
+    
+
+
+
+
+//////////////////////////////////
+//--Script de PAGINA PRINCIPAL--//
+
+tablaTareasDiarias = $("#tablaTareasDiarias").DataTable({
+    "columnDefs":[{
+     "targets": -1,
+     "data":null,
+     "defaultContent": "<div class='text-center'><div class='btn-group'><button class='btn btn-primary btnEditarTarea'>Editar Tarea</button></div>"  
+    }],
+     
+ "language": {
+         "lengthMenu": "Mostrar _MENU_ registros",
+         "zeroRecords": "No se encontraron resultados",
+         "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+         "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+         "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+         "sSearch": "Buscar:",
+         "oPaginate": {
+             "sFirst": "Primero",
+             "sLast":"Último",
+             "sNext":"Siguiente",
+             "sPrevious": "Anterior"
+          },
+          "sProcessing":"Procesando...",
+     }
+ });
+
+
+
+$("#btnNuevaTarea").click(function(){
+    $("#formTareas").trigger("reset");
+    $(".modal-header").css("background-color", "#1cc88a");
+    $(".modal-header").css("color", "white");
+    $(".modal-title").text("Nueva Tarea");            
+    $("#modalCRUD").modal("show");        
+    id=null;
+    opci = 1; //alta
+});    
+
+//botón EDITAR    
+$(document).on("click", ".btnEditarTarea", function(){
+    fila = $(this).closest("tr");
+
+    tipologia = fila.find('td:eq(1)').text();
+    cuadrilla = fila.find('td:eq(3)').text();
+    descripcion = fila.find('td:eq(5)').text();
+
+    console.log( tipologia);
+
+    $("#tipoloDrop").val(tipologia);//setea el valor
+    $("#descTarea").val(descripcion);
+    $("#cuadrillaDrop").val(cuadrilla);
+    
+    //console.log($("#tipoloDrop").val());
+
+    opci = 2; //editar
+    
+    $(".modal-header").css("background-color", "#4e73df");
+    $(".modal-header").css("color", "white");
+    $(".modal-title").text("Editar Tarea");            
+    $("#modalCRUD").modal("show");  
+    
+});
+
+$("#formTareas").submit(function(e){
+    e.preventDefault(); 
+    
+    tipologia = $.trim($("#tipoloDrop").val());   
+    descripcion = $.trim($("#descTarea").val());   
+    cuadrilla = $.trim($("#cuadrillaDrop").val());   
+    //tareaID = $.trim($("#cuadrillaDrop").val()); 
+
+    console.log(tipologia + " | " + descripcion + " | " + cuadrilla + " | " + opci);
+
+    $.ajax({
+        url: "bd/crudTareaDiaria.php",
+        type: "POST",
+        dataType: "json",
+        data: {tipologia:tipologia, descripcion:descripcion, cuadrilla:cuadrilla, opci:opci},
+        
+        success: function(data){  
+            console.log(data);
+
+            /*
+            id = data[0].id;            
+            nombre = data[0].nombre;
+            pais = data[0].pais;
+            edad = data[0].edad;
+
+            if(opci == 1){tablaPersonas.row.add([id,nombre,pais,edad]).draw();}
+            else{tablaPersonas.row(fila).data([id,nombre,pais,edad]).draw();}       */     
+        },
+        error: function (jqXHR, exception) {
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Not connect.\n Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            console.log(msg);
+        },   
+    });
+
+    $("#modalCRUD").modal("hide");    
+    
+});    
+
+
+});
